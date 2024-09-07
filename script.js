afc07c3f7ed17e045ea8ea932b4597733debacbf
ttv-shanu69 <156026347+ttv-shanu69@users.noreply.github.com>
2024-09-07 10:19:39 -0700

    My Portfolio

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed (".my-text" , {
+    strings: ["Front-end Developer." , "Full-stack Developer." , "Web Designer."],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true
+}); 
